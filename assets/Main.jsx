4d8d39ddd90cf7d815a6fc960b38820752f1a015
Dominik Jakobsze <dominikjakobsze@Dominiks-Air.lan>
2023-01-29 22:44:36 +0100

    react entrypoint

---
+++ assets/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <>
+            <div className="text-3xl font-bold underline">
+                Hello world!
+            </div>
+        </>
+    );
+};
+
+export default Main;
--- assets/app.js
+++ assets/app.js
@@ -5,4 +5,10 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import Main from "./Main";
+
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById('react-app')).render(<Main/>);
