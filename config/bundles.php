f6d31fbd64b627e793256143813f5d446b5cc42a
Dominik Jakobsze <dominikjakobsze@Dominiks-Air.lan>
2023-01-29 22:20:07 +0100

    mercure added

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
