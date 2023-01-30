ce996289f235dbf98423dcffe662b822bef877ea
Dominik Jakobsze <dominikjakobsze@Dominiks-Air.lan>
2023-01-30 18:46:07 +0100

    Register Form added

--- src/Form/RegisterForm.php
+++ src/Form/RegisterForm.php
@@ -3,12 +3,32 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-      $builder
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Email()
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 8,
+                        'max' => 60
+                    ])
+                ]
+            ]);
     }
 }
