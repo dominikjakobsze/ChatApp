7aa7c1dd6920755fddc94c779877ea826e3372a1
Dominik Jakobsze <dominikjakobsze@Dominiks-Air.lan>
2023-01-30 18:44:06 +0100

    added form builder

--- src/Form/RegisterForm.php
+++ src/Form/RegisterForm.php
@@ -2,7 +2,13 @@
 
 namespace App\Form;
 
-class RegisterForm
-{
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 
+class RegisterForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+      $builder
+    }
 }
