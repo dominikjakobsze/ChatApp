007f35a433d32b6c285f0177f9b98ba4023d3c22
Dominik Jakobsze <dominikjakobsze@Dominiks-Air.lan>
2023-01-30 18:30:02 +0100

    define entrypoint for react router => exclude symfony except /api

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/{react}', name: 'react_entrypoint', requirements: ['react' => '^(?!api).*$'], defaults: ['react' => null])]
+    public function home(): Response
+    {
+        return $this->render('main.html.twig');
+    }
+}
