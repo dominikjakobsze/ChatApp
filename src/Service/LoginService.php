a00a5ccfaee7a6f055e52ff63aa687d47d66892d
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-01-31 16:54:01 +0100

    fixes in LoginService method => getUserFromEmail, new return type

--- src/Service/LoginService.php
+++ src/Service/LoginService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Entity\Auth;
 use App\Repository\AuthRepository;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
@@ -16,13 +17,14 @@ class LoginService
 
     /**
      * @param string $email
-     * @return bool
+     * @return Auth
      */
-    public function getUserFromEmail(string $email): bool
+    public function getUserFromEmail(string $email): Auth
     {
-        if ($this->authRepository->findOneBy(['email' => $email]) == null) {
+        $auth = $this->authRepository->findOneBy(['email' => $email]);
+        if ($auth == null) {
             throw new BadRequestHttpException("Such email does not exist");
         }
-        return true;
+        return $auth;
     }
 }
