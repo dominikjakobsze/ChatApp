2cffca11cee4a49749340ccaf310f8098f21ddc2
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-02-01 19:46:09 +0100

    PHPUnit test: EmailDoesNotExist

--- tests/Service/RegisterServiceTest.php
+++ tests/Service/RegisterServiceTest.php
@@ -5,22 +5,40 @@ namespace App\Tests\Service;
 use App\Repository\AuthRepository;
 use App\Service\RegisterService;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class RegisterServiceTest extends TestCase
 {
-    private $registerService;
-    private $authRepositoryMock;
+    private RegisterService $registerService;
+    private AuthRepository $authRepositoryMock;
 
+    /**
+     * @return void
+     */
     public function setUp(): void
     {
         $this->authRepositoryMock = $this->createMock(AuthRepository::class);
         $this->registerService = new RegisterService($this->authRepositoryMock);
     }
 
-    public function testEmailExists(): void
+    /**
+     * @return void
+     */
+    public function testEmailDoesNotExist(): void
     {
         $email = "dominik@wp.pl";
         $this->authRepositoryMock->method('findOneBy')->with(['email' => $email])->willReturn(null);
         $this->assertFalse($this->registerService->checkIfEmailIsTaken($email));
+    }
+
+    /**
+     * @return void
+     */
+    public function testEmailExists(): void
+    {
+        $email = "dominik@wp.pl";
+        $this->authRepositoryMock->method('findOneBy')->with(['email' => $email])->willReturn(true);
+        $this->expectException(BadRequestHttpException::class);
+        $this->registerService->checkIfEmailIsTaken($email);
     }
 }
